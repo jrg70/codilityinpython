e8ed0e111f024d727e1ea2fed68aaaf0bfb9f481
Jan Gröls <90774019+jrg70@users.noreply.github.com>
2022-07-01 13:24:52 +0100

    Update and rename ex-8-2-EquiLeader.py to ex-8-2-equiLeader.py

--- ex-8-2-equiLeader.py
+++ ex-8-2-equiLeader.py
@@ -40,4 +40,33 @@ N is an integer within the range [1..100,000];
 each element of array A is an integer within the range [−1,000,000,000..1,000,000,000]."""
 
 def solution(A):
+  if len(A) == 1:   # if only one element, it is the dominator itself
+    return(0)
+  
+  Copy = A.copy()
+  Copy.sort()
+  occ = 1
+  n = 0
+  leader = 0
+  
+  for i in range(0,len(Copy)-1):
+    if Copy[i] != Copy[i+1]: #in the sorted list different numbers are listed
+      occ = 1
+    else: # the same number is twice add 1 to the occurrence
+      occ+=1
+    if occ > int(len(Copy)/2):
+      leader = Copy[i]
+      break
+  for i in range(0,len(A)):
+    if A[i] == leader:
+        n+=1
+  Eleader = 0
+  occ = 0
+  for i in range(0,len(A)):
+    if A[i] == leader:
+        occ += 1
+    if (i+1<2*occ and len(A)-i-1 < 2*(n-occ)):
+        Eleader +=1
+  return Eleader
+  return -1
   
