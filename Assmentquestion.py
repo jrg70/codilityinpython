e0482b9a0bcfe9e80060bb80eb8aa1f0ca4700e8
Jan Gröls <90774019+jrg70@users.noreply.github.com>
2022-07-12 09:44:33 +0100

    Update Assmentquestion.py

--- Assmentquestion.py
+++ Assmentquestion.py
@@ -1,4 +1,6 @@
-# Three questions in total: The last one was to compute the specifity/sensitivity based on two binary strings:
+# Three questions in total
+
+# 1. Compute the specifity/sensitivity based on two binary strings:
 
 def solution(A,B,q):
   if q == True: # spec
@@ -19,3 +21,16 @@ def solution(A,B,q):
       elif A[i] == 1 and B[i] == 0:
         FN += 1
     return TP/(TP+FN)
+
+  # 2. Compute the letter that appears twice in a word unde rthe assumption all are disticnt expect one, i.e. "codility" computes "i" or "aba" computes "a".
+  
+  def solution(S):
+  dict = {}
+  for i in range(0,len(S)):
+    if S[i] not in dict:
+      dict[S[i]] = 0
+    else:
+      dict[S[i]] += 1
+  return dict
+
+solution("aba")
