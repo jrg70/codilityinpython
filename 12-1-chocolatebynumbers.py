365279c0cded36cd26cb897ec7c28402f5c4a3e5
Jan Gröls <90774019+jrg70@users.noreply.github.com>
2022-07-12 09:34:19 +0100

    Update 12-1-chocolatebynumbers.py

--- 12-1-chocolatebynumbers.py
+++ 12-1-chocolatebynumbers.py
@@ -24,3 +24,12 @@ Write an efficient algorithm for the following assumptions:
 
 N and M are integers within the range [1..1,000,000,000]."""
 
+def gcd(a,b):
+  if a%b == 0:
+    return b
+  else:
+    return gcd(b,a%b)
+
+def solution(N,M):
+  return int(N/gcd(N,M))
+solution(1,2)
